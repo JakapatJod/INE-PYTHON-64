bd8604bb04c1edc8a0e6fe8035910cc3ebe68d49
JakapatJod <jakapat2332@gmail.com>
2021-07-20 10:44:57 +0700

    add new Test

--- Test.py
+++ Test.py
@@ -1,2 +1,14 @@
 print("Hello World")
 print("Jakapat Jodduangchan")
+print("100.00")
+from math import pi
+print('%.2f'%pi)
+print("I'm bat man and superman")
+from math import pi
+print('%.4f'%pi)
+print('10+14 =' +str(10+14))
+print('14-10 = %.2f' %(14-10))
+print('ta\ta\na\a\ta')
+print('90.146-89.146 = %.2f' %(90.146-89.146))
+print("I'm Mob"+ " I'm Sutip")
+print('%d %d %d %d %d' %(1,2,65,95,100))
