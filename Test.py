519094003491f63bb3c7e122e2e14279782a71c9
JakapatJod <jakapat2332@gmail.com>
2021-07-20 15:14:13 +0700

    New Test

--- Test.py
+++ Test.py
@@ -1,8 +1,2 @@
-
-name = input('What is your name ? ')
-age = int (input('What us your age ? '))
-income = float(input ('What is your income ? '))
-print ('Here is the data you entered: ')
-print ('Name:' ,name)
-print ('Age :' , age)
-print ('Income:' ,format(income , '12,.2f'))
+print('Game')
+print('Hello World')
