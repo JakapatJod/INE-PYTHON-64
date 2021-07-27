6673ee636d2fc47a30acc709ac3e509ee66b1490
JakapatJod <jakapat2332@gmail.com>
2021-07-27 10:27:02 +0700

    Move

--- Test.py
+++ Test.py
@@ -1,2 +1,18 @@
 print('Game')
 print('Hello World')
+integer = 10.02
+flost = 4.98
+print('integer = %d flost = %d' %(integer,flost))
+print('9.04+10.00 = %.2f' %(9.04+10.00))
+from math import pi
+print('%.4f'%pi)
+x = 40.99
+y = 10.44
+print('40.99 + 10.44 = ',(40.99 + 10.44))
+print('40.99 - 10.44 = ',(40.99 - 10.44))
+print('40.99 * 10.44 = ',(40.99 * 10.44))
+print('40.99 / 10.44 = ',(40.99 / 10.44))
+print('40.99 // 10.44 = ',(40.99 // 10.44))
+print('40.99 ** 10.44 = ',(40.99 ** 10.44))
+print('40.99 % 10.44 = ',(40.99 % 10.44))
+print("I'm superman")
