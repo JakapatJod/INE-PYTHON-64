f3d810c1c0b65ccbebab66d70a242ccaf04ff317
JakapatJod <jakapat2332@gmail.com>
2021-08-01 16:02:19 +0700

    up

--- Test02.py
+++ Test02.py
@@ -1,4 +1,7 @@
+print('--------------')
 print('KPH \tMPH')
+print('--------------')
 for kph in range(60,131):
     mph = (kph*0.621) 
     print(kph,'\t', format(mph,',.1f'))
+print('--------------')
