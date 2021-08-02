622e258a7c8d3e32f18e4a536e1d888adaecfdfd
JakapatJod <jakapat2332@gmail.com>
2021-08-02 21:04:57 +0700

    up

--- Homework/Homework_ex04.py
+++ Homework/Homework_ex04.py
@@ -6,16 +6,16 @@ print('4. Divide')
 op = int(input('Select operation form 1, 2, 3, 4 : '))
 number1 = int(input('Enter first number = '))
 number2 = int(input('Enter second number = '))
-if op <= 1 :
+if op == 1 :
     calculate =number1+number2
     print(number1,'+',number2 ,'=',(calculate))
-elif op <=2 :
+elif op == 2 :
     calculate =number1-number2
     print(number1,'-',number2 ,'=',(calculate))
-elif op <=3 :
+elif op ==3 :
     calculate =number1*number2
     print(number1,'*',number2 ,'=',(calculate))
-elif op <=4 :
+elif op ==4 :
     calculate =number1/number2
     print(number1,'/',number2 ,'=',(calculate))
 else:
