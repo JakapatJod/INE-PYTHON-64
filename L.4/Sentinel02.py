85a9b698fca864cb4b038a0fc8497022bf0f4d81
JakapatJod <jakapat2332@gmail.com>
2021-08-03 14:19:44 +0700

    p

--- L.4/Sentinel02.py
+++ L.4/Sentinel02.py
@@ -2,5 +2,5 @@ row = int(input('How many rows ? '))
 columns = int(input('How many columms ? '))
 for call in range(row):
     for k in range(columns):
-        print('*',end='')
+        print('*',end='') 
     print("")
