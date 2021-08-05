d83f58bc757908b4c4a81cce752cd68a9ef17972
JakapatJod <jakapat2332@gmail.com>
2021-08-05 17:59:37 +0700

    up

--- Homework/Homework_02.py
+++ Homework/Homework_02.py
@@ -1,8 +1,10 @@
-columns = int(input('How many columns ? : '))
-b = int(100/columns)
-o = 1
-for d in range(b):
-    for a in range(columns):
-        print(o,end='\t')
-        o = o + 1 
+print('\t \t \t \t \t1-100')
+columns = int(input('\t \t \t \t \tHow many columns = '))
+z = 1
+while (z <= 100):
+    for c in range(columns):
+        print(z,end='\t')
+        z = z + 1
+        if z > 100:
+            break
     print('')
