2118306688fc1968a42d661813336b3f56ad2b85
JakapatJod <jakapat2332@gmail.com>
2021-08-10 13:42:50 +0700

    up

---
+++ L.5/basic_string_method04.py
@@ -0,0 +1,7 @@
+balloon = '         Anirach has a balloon       '
+print('[',balloon.strip(),']')
+print('[',balloon.lstrip(),']')
+print('[',balloon.rstrip(),']')
+
+print('[',balloon.strip('#'),']')
+print('[',balloon.rstrip('#'),']')
