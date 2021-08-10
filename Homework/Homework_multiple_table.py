f5a77e532c293c6a9eed54224d79fb64d9ba4c35
JakapatJod <jakapat2332@gmail.com>
2021-08-10 20:56:06 +0700

    l

--- Homework/Homework_multiple_table.py
+++ Homework/Homework_multiple_table.py
@@ -1,11 +1,11 @@
 print('Multiply Table')
-Ferrari = 'Y'    
-while Ferrari == 'Y':    
-    Toyota = int(input('Enter the first number = '))
-    Ducati = int(input('Enter the second number = '))
-    BMW = int(input('Enter the third number = '))
+Ferrari = 'y'    
+while Ferrari == 'y':    
+    Toyota = int(input('\tEnter the first number = '))
+    Ducati = int(input('\tEnter the second number = '))
+    BMW = int(input('\tEnter the third number = '))
     print('')
     for Tesla in range(1,13):
         print(Toyota,'*',Tesla,'=',Toyota*Tesla,'\t',Ducati,'*',Tesla,'=',Ducati*Tesla,'\t',BMW,'*',Tesla,'=',BMW*Tesla)
     print('')
-    Ferrari = input('Do you want to calculate again ? (Enter Y for yes): ')
+    Ferrari = input('Do you want to calculate again ? (Enter y for yes): ')
