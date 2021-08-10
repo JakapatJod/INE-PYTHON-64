b5507d8deb61a1bf0bb55e0bab2031cb8a82e080
JakapatJod <jakapat2332@gmail.com>
2021-08-10 10:58:32 +0700

    up

---
+++ L.5/work01.py
@@ -0,0 +1,5 @@
+f = (input('Enter your first name: '))
+l = (input('Enter your last name: '))
+p = (input('Enter your student ID numbers: '))
+print('Your system login name is: ')
+print((f[:3])+(l[:3])+(p[5:8]))
