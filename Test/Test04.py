c150a8612914f9cfe0755495f23dd541763547c8
JakapatJod <jakapat2332@gmail.com>
2021-08-12 17:48:52 +0700

    up

--- Test/Test04.py
+++ Test/Test04.py
@@ -1,11 +1,13 @@
 print('Multiply Table')
-Ferrari = 'Y'    
-while Ferrari == 'Y':    
-    Toyota = int(input('Enter the first number = '))
-    Ducati = int(input('Enter the second number = '))
-    BMW = int(input('Enter the third number = '))
+Ferrari = 'y'    
+while Ferrari == 'y':    
+    Toyota = int(input('\tEnter the first number = '))
+    Ducati = Toyota + 1
+    BMW = Toyota + 2
+    print('-'*45)
     print('')
     for Tesla in range(1,13):
-        print(Toyota,'*',Tesla,'=',Toyota*Tesla,'\t',Ducati,'*',Tesla,'=',Ducati*Tesla,'\t',BMW,'*',Tesla,'=',BMW*Tesla)
+        print('|','\t',Toyota,'*',Tesla,'=',Toyota*Tesla,'\t','|','\t',Ducati,'*',Tesla,'=',Ducati*Tesla,'\t','|','\t',BMW,'*',Tesla,'=',BMW*Tesla,'\t','|')
     print('')
-    Ferrari = input('Do you want to calculate again ? (Enter Y for yes): ')
+    print('-'*45)
+    Ferrari = input('Do you want to calculate again ? (Enter y for yes): ')
