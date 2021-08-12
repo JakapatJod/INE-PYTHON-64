1bed6cafd725056083c6fecd430986b31382f451
JakapatJod <jakapat2332@gmail.com>
2021-08-12 20:43:46 +0700

    up

--- Homework/Homework_multiple_table.py
+++ Homework/Homework_multiple_table.py
@@ -1,13 +1,15 @@
-print('Multiply Table')
+print('\t\t\t\tMultiply Table')
 Ferrari = 'y'    
 while Ferrari == 'y':    
-    Toyota = int(input('\tEnter the first number = '))
+    Toyota = int(input('\t\t\t\tEnter the number = '))
     Ducati = Toyota + 1
     BMW = Toyota + 2
-    print('-'*45)
+    print('-'*80)
     print('')
     for Tesla in range(1,13):
-        print('|','\t',Toyota,'*',Tesla,'=',Toyota*Tesla,'\t','|','\t',Ducati,'*',Tesla,'=',Ducati*Tesla,'\t','|','\t',BMW,'*',Tesla,'=',BMW*Tesla,'\t','|')
+        print('|','\t',Toyota,'*',Tesla,'=',Toyota*Tesla,'\t','|','\t'
+        ,Ducati,'*',Tesla,'=',Ducati*Tesla,'\t','|','\t'
+        ,BMW,'*',Tesla,'=',BMW*Tesla,'\t','|')
     print('')
-    print('-'*45)
+    print('-'*80)
     Ferrari = input('Do you want to calculate again ? (Enter y for yes): ')
--- Test/Test04.py
+++ Test/Test04.py
@@ -7,7 +7,8 @@ while Ferrari == 'y':
     print('-'*45)
     print('')
     for Tesla in range(1,13):
-        print('|','\t',Toyota,'*',Tesla,'=',Toyota*Tesla,'\t','|','\t',Ducati,'*',Tesla,'=',Ducati*Tesla,'\t','|','\t',BMW,'*',Tesla,'=',BMW*Tesla,'\t','|')
+        print('|','\t',Toyota,'*',Tesla,'=',Toyota*Tesla,'\t','|','\t'
+        ,Ducati,'*',Tesla,'=',Ducati*Tesla,'\t','|','\t',BMW,'*',Tesla,'=',BMW*Tesla,'\t','|')
     print('')
     print('-'*45)
     Ferrari = input('Do you want to calculate again ? (Enter y for yes): ')
