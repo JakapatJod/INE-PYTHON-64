d25928b1eff926989d0cfb41044f51d7948b6187
JakapatJod <jakapat2332@gmail.com>
2021-08-17 09:40:34 +0700

    up

---
+++ L.6/Function01.py
@@ -0,0 +1,7 @@
+def message():
+    print('I am Jakapat')
+    print('I Love Python.')
+#Call the message function.
+message()
+#จะเรียก function มาอีกรอบ
+message()
---
+++ L.6/Function02.py
@@ -0,0 +1,11 @@
+def main ():
+    print('I have a message for you')
+    message()
+    print('Goodbye!')
+
+#Next we define the message function
+def message():
+    print('I am Jakapat')
+    print('I Love Python.')
+#Call the main function.
+main()
