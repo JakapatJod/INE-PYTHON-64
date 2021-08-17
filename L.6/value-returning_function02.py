e00a4d992984289aae0d79caa4d2e7f700528d01
JakapatJod <jakapat2332@gmail.com>
2021-08-17 13:12:48 +0700

    up

--- L.6/value-returning_function02.py
+++ L.6/value-returning_function02.py
@@ -1,8 +1,8 @@
 def get_name():
     first = input('Enter you first name: ')
     last = input('Enter your last name: ')
-
+    #Return both name. ส่งคืนทั้งสองชื่อ
     return first, last
 
-first_name,last_name = get_name()
+first_name,last_name = get_name() #สร้างตัวแปร ชื่อว่า first_name กับ last_name
 print('First name: ',first_name,'Last name: ',last_name)
