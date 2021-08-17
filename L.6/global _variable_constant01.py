cb5622b2d8ec3f8a8d5aaa2ea403e9cfafcc0edd
JakapatJod <jakapat2332@gmail.com>
2021-08-17 10:53:59 +0700

    up

--- L.6/global _variable_constant01.py
+++ L.6/global _variable_constant01.py
@@ -1,6 +1,6 @@
 number = 0
 def main():
-    global number
+    global number #เรียกใช้ตัวแปรนอกฟังก์ชันที่ชื่อว่า number ถ้าลบออกไปจะกลาย 0 ทันที
     number = int(input('Enter a number: '))
     show_number()
 
