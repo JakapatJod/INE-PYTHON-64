5b33cd08d697fa27d1bc41380fd9c9fd46ee0fea
JakapatJod <jakapat2332@gmail.com>
2021-08-17 10:54:04 +0700

    up

---
+++ L.6/global _variable_constant01_1.py
@@ -0,0 +1,10 @@
+number = 0
+def main():
+    number = int(input('Enter a number: '))
+    show_number()
+    print('number is ',number)
+
+def show_number():
+    print('The number you entered is',number)
+
+main()
