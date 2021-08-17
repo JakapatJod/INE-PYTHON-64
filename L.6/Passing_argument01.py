78865edb14b4dcb8f85ee7494fc2f36084ef15b6
JakapatJod <jakapat2332@gmail.com>
2021-08-17 09:58:09 +0700

    up

---
+++ L.6/Passing_argument01.py
@@ -0,0 +1,10 @@
+def main():
+    value = 40
+    show_double(value)
+#เรียก function show_double(value) มาใช้
+def show_double(number):
+    result = number * 2
+    print('')
+    print(result)
+    print('')
+main()
