4cbda56ff6b4a4b97789438290dee4208c70668d
JakapatJod <jakapat2332@gmail.com>
2021-08-17 13:06:47 +0700

    up

--- L.6/value-returning_function02.py
+++ L.6/value-returning_function02.py
@@ -1,3 +1,8 @@
 def get_name():
     first = input('Enter you first name: ')
     last = input('Enter your last name: ')
+
+    return first, last
+
+first_name,last_name = get_name()
+print('First name: ',first_name,'Last name: ',last_name)
