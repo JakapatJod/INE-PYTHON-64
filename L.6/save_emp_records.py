64c988cbe717a854830bed24aa73f8f8ae081f57
JakapatJod <jakapat2332@gmail.com>
2021-08-24 13:55:52 +0700

    up

---
+++ L.6/save_emp_records.py
@@ -0,0 +1,31 @@
+def main():
+    # Get the number of employee records to create.
+    num_emps = int(input('How mant employee records ' + \
+                    'do you want to create ? '))
+    
+    # Open the file writting.
+    emp_file = open('employee.txt','w')
+
+    # Get each employee's data and write it to
+    # the file.
+    for count in range(1,num_emps + 1):
+        # Get the data for an employee.
+        print('Enter data for employee #', count, sep='')
+        name = input('Name: ')
+        id_num = input('ID Number: ')
+        dept = input('Department: ')
+        
+        # Write the data as a records to the file.
+        emp_file.write(name + '\n')
+        emp_file.write(id_num + '\n')
+        emp_file.write(dept + '\n')
+        
+        # Display a blank line.
+        print()
+
+    # Close the file.
+    emp_file.close()
+    print('Employee records written to employee.txt.')
+
+# Call the main function
+main()
