a8b1a2b2b0943a6e102a9f2b6be3ea195ae19aad
JakapatJod <jakapat2332@gmail.com>
2021-08-24 10:19:30 +0700

    up

--- L.6/file_read.py
+++ L.6/file_read.py
@@ -4,10 +4,11 @@ def main():
 
     #Read the file's contents. 
     file_contents = infile.read()
-
+    #Close the file.
     infile.close()
 
-    
+    #Print the data that was read into
+    # memory.
     print(file_contents)
-
+#Call the main function.
 main()
