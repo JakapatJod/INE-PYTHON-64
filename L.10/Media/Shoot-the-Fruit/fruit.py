410bdb057bf6bbd5b740e20c2bc18fefdcdee3fd
JakapatJod <jakapat2332@gmail.com>
2021-09-28 10:43:34 +0700

    ีีup

--- L.10/Media/Shoot-the-Fruit/fruit.py
+++ L.10/Media/Shoot-the-Fruit/fruit.py
@@ -1,5 +1,6 @@
 # Game Shoot the fruit.
 import pgzrun
+from random import randint
 
 # define size of the windowns
 WIDTH = 640
@@ -13,4 +14,19 @@ def draw():
     screen.clear()
     apple.draw()
 
+# function random position of apple
+def place_apple():
+    apple.x = randint(10,600)
+    apple.y = randint(10,400)
+
+# function to get mouse event
+def on_mouse_down(pos):
+    if apple.collidepoint(pos):
+        print('Good Shot!')
+        place_apple()
+    else:
+        print('You missed!')
+
+
+place_apple()
 pgzrun.go()
