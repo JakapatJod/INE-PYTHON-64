cf5dd114b40332a9d1d0b3231aceef07868b3f13
JakapatJod <jakapat2332@gmail.com>
2021-09-28 12:29:04 +0700

    up

--- L.10/Media/Coin-Collector/coins.py
+++ L.10/Media/Coin-Collector/coins.py
@@ -6,6 +6,9 @@ from random import randint
 WIDTH = 800
 HEIGHT = 600
 
+# define variable
+score = 0
+
 # Create object
 fox = Actor('fox')
 fox.pos = 100, 100
@@ -16,24 +19,41 @@ def draw():
     screen.fill('blue')
     fox.draw()
     coin.draw()
-
+    screen.draw.text('Score : '+str(score),color='red',topleft=(10,10))
 
 def place_coin():
     coin.x = randint(20,(WIDTH - 20))
     coin.y = randint(20,(HEIGHT - 20))
 
 def update():
-    if keyboard.left:
-        fox.x = fox.x - 2
-    elif keyboard.right:
-        fox.x = fox.x + 2
-    elif keyboard.up:
-        fox.y = fox.y - 2
-    elif keyboard.down:
-        fox.y = fox.y + 2
+    global score
+    if keyboard.left :
+        fox.x = fox.x - 4
+       
+    elif keyboard.right  :
+        fox.x = fox.x + 4
+        
+    elif keyboard.up  :
+        fox.y = fox.y - 4
+        
+    elif keyboard.down :
+        fox.y = fox.y + 4
+    
+    elif fox.x > (800):
+        fox.x = (1)
+    
+    elif fox.x < (1):
+        fox.x = (800)
+    
+    elif fox.y > (600):
+        fox.y = (1)
+    
+    elif fox.y < (1):
+        fox.y = (600)
     
     coin_collected = fox.colliderect(coin)
     if coin_collected:
         place_coin()
+        score += 1
 
 pgzrun.go()
